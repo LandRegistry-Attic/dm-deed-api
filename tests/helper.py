eb4b23b45ff0afa80e4095af1d99679a734eb57c
Ramin Vazir <ramin.vazir@landregistry.gsi.gov.uk>
2015-11-17 12:17:46 +0000

    Revert "Us5 provide borrower names"

--- tests/helper.py
+++ tests/helper.py
@@ -4,32 +4,6 @@ class DeedModelMock:
 
 
 class DeedHelper:
-    _json_doc = {
-        "title_number": "DN100",
-        "borrowers": [
-            {
-                "forename": "fred",
-                "middle_name": "joe",
-                "surname": "bloggs"
-            },
-            {
-                "forename": "lisa",
-                "surname": "bloggette"
-            }
-        ]
-    }
+    _json_doc = {"title_number": "DN100"}
 
-    _invalid_title = {
-        "title_number": "BBBB12313212BB",
-        "borrowers": [
-            {
-                "forename": "fred",
-                "middle_name": "joe",
-                "surname": "bloggs"
-            },
-            {
-                "forename": "lisa",
-                "surname": "bloggette"
-            }
-        ]
-    }
+    _invalid_title = {"title_number": "BBBB12313212BB"}
