879dee9b8de6671e79d360e9e60516112179069a
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-01-05 13:59:48 +0000

    change of new_deed_json variable to updated

--- application/deed/views.py
+++ application/deed/views.py
@@ -103,44 +103,43 @@ def delete_borrower(borrower_id):
 def get_existing_deed_and_update(deed_reference):
 
     # Firstly check payload coming in is valid:
-    new_deed_json = request.get_json()
+    updated_deed_json = request.get_json()
     borrowerService = BorrowerService()
 
-    error_count, error_message = validate_helper(new_deed_json)
+    error_count, error_message = validate_helper(updated_deed_json)
 
     if error_count > 0:
         return error_message, status.HTTP_400_BAD_REQUEST
     else:
-        # If Valid:
-        # Get Current Deed
+        # If Valid: - Get Current Deed
         result = Deed.query.filter_by(token=str(deed_reference)).first()
 
         #  Get existing borrowers and loop through to get
         #  the ID's that the pasted JSON will overwrite
         existing_deed_borrowers = result.deed['borrowers']
         for (i, existing_borrower) in enumerate(existing_deed_borrowers):
-            new_deed_json['borrowers'][i]['id'] = existing_borrower['id']
+            updated_deed_json['borrowers'][i]['id'] = existing_borrower['id']
 
     if result is None:
         abort(status.HTTP_404_NOT_FOUND)
     else:
 
-        result.deed = new_deed_json
+        result.deed = updated_deed_json
         json_doc = {
-            "title_number": new_deed_json['title_number'],
-            "md_ref": new_deed_json['md_ref'],
+            "title_number": updated_deed_json['title_number'],
+            "md_ref": updated_deed_json['md_ref'],
             "borrowers": []
             }
 
         # Make a deed out of new information
-        valid_dob_result = _(new_deed_json['borrowers']).chain()\
+        valid_dob_result = _(updated_deed_json['borrowers']).chain()\
             .map(lambda x, *a: x['dob'])\
             .reduce(valid_dob, True).value()
 
         if not valid_dob_result:
             abort(status.HTTP_400_BAD_REQUEST)
 
-        phone_number_list = _(new_deed_json['borrowers']).chain()\
+        phone_number_list = _(updated_deed_json['borrowers']).chain()\
             .map(lambda x, *a: x['phone_number'])\
             .value()
 
@@ -148,7 +147,7 @@ def get_existing_deed_and_update(deed_reference):
             abort(status.HTTP_400_BAD_REQUEST)
 
         try:
-            for borrower in new_deed_json['borrowers']:
+            for borrower in updated_deed_json['borrowers']:
                 borrower_json = {
                     "id": borrower['id'],
                     "forename": borrower['forename'],
